7b266819a226ce471007e10d28e8248bf07ff1f4
kaifengQu <kaifeng.qu@163.com>
2023-12-12 09:52:15 +0800

    V1.5 TB okk new Structure temp okk

--- rnnForYawrate.py
+++ rnnForYawrate.py
@@ -21,9 +21,9 @@ class CustomDataset_RNN(Dataset):
                 self.samples.append(data.copy())
                 data.clear()
     def __len__(self):
-        return len(self.samples)-6# -6 : -1 is special for my case and -5 is sequence length
+        return len(self.samples)-11# -6 : -1 is special for my case and -5 is sequence length
     def __getitem__(self, idx):
-        sequence = self.samples[idx:idx+5] # e.g. 0 1 2 3 4 shape is (5,9)(squence length:5,features:9)
+        sequence = self.samples[idx:idx+10] # e.g. 0 1 2 3 4 shape is (5,9)(squence length:5,features:9)
         features=[]
         # for i in range (5):
         #     sample=sequence[i]
@@ -41,15 +41,15 @@ class CustomDataset_RNN(Dataset):
             ])# dim 1
             features.append(feature)#dim 0 a list to tensor with shape (5,9)
         features = torch.stack(features, dim=0)
-        sequence2 = self.samples[idx+6]# +1 is special process for my case
+        sequence2 = self.samples[idx+10]# +1 is special process for my case
         labels=torch.tensor([
         sequence2.get('msg.egoEgoStatus.yawRate', 0.0),  
         sequence2.get('msg.egoEgoStatus.linearSpeed', 0.0),
         sequence2.get('msg.egoEgoStatus.accerationX', 0.0),
         sequence2.get('msg.egoEgoStatus.accerationY', 0.0),
-        sequence2.get('msg.egoEgoStatus.steerWheelAngle', 0.0),
+        #sequence2.get('msg.egoEgoStatus.steerWheelAngle', 0.0),
         #sequence2.get('msg.egoEgoStatus.steerWheelAngleRate', 0.0),
-        sequence2.get('msg.egoEgoStatus.frontWheelAngle', 0.0)
+        #sequence2.get('msg.egoEgoStatus.frontWheelAngle', 0.0)
         ])
         labels=labels.view(1,-1)# or .unsqueeze(0) (7) to (1,7)
         return features,labels
@@ -60,11 +60,13 @@ class MyRNN(nn.Module):
         self.linear_in = nn.Linear(6,32)
         self.relu = nn.ReLU()
         self.linear_32=nn.Linear(32,32)
-        self.rnn = nn.RNN(input_size=32, hidden_size=32, num_layers=1, batch_first=True) 
-        self.linear_out=nn.Linear(32,6)
+        self.rnn = nn.RNN(input_size=6, hidden_size=32, num_layers=1, batch_first=True) 
+        self.linear_out=nn.Linear(32,4)
         # 创建 Batch Normalization 层
-        self.batch_norm_layer = nn.BatchNorm1d(num_features=32,affine=True, track_running_stats=False)
+        #self.batch_norm_layer = nn.BatchNorm1d(num_features=32,affine=True, track_running_stats=False)
         self.dropout = nn.Dropout(p=0.2)  # 添加 Dropout 层，丢弃率0.2
+        # setup LN
+        self.layer_norm = nn.LayerNorm(normalized_shape=32)
         for name, param in self.named_parameters():
             if 'weight' in name:
                 nn.init.normal_(param, mean=0.0, std=0.01)
@@ -72,18 +74,23 @@ class MyRNN(nn.Module):
                 nn.init.constant_(param, 0)         
     def forward(self, x,hidden_prev):
         batch_size,sequence_length,num_features=x.shape
-        temp1=self.linear_in(x)# temp1 (16,5,32)
-        #temp1 = temp1.unsqueeze(2)  # 增加一个维度，变成 (batch_size, sequence_length, 1, input_size)
-        temp1 = temp1.view(-1, 32)  # 将形状从 (batch_size, sequence_length, num_features) 改为 (batch_size*sequence_length, num_features) 
-        temp11 = self.batch_norm_layer(temp1)
-        temp11 = temp11.view(batch_size,sequence_length, 32)  # 再次变换形状回到原来的状态
-        #temp1 = temp11.squeeze(2)  # 移除添加的维度
-        temp2=self.relu (temp11)
-        temp3=self.linear_32(temp2)# temp3 (16,5,32)
-        out, hidden_prev = self.rnn(temp3,hidden_prev)# out (16,5,32) #hidden(1,16,32)        
+
+        # temp1=self.linear_in(temp01)# temp1 (16,5,32)
+        # #temp1 = temp1.unsqueeze(2)  # 增加一个维度，变成 (batch_size, sequence_length, 1, input_size)
+        # # temp1 = temp1.view(-1, 32)  # 将形状从 (batch_size, sequence_length, num_features) 改为 (batch_size*sequence_length, num_features) 
+        # # temp11 = self.batch_norm_layer(temp1)
+        # # temp11 = temp11.view(batch_size,sequence_length, 32)  # 再次变换形状回到原来的状态
+        # #temp1 = temp11.squeeze(2)  # 移除添加的维度
+ 
+        # temp3=self.linear_32(temp2)# temp3 (16,5,32)
+        out, hidden_prev = self.rnn(x,hidden_prev)# out (16,5,32) #hidden(1,16,32)        
         # Get output from the last time step
         last_output = out[:, -1, :]#last_output(16,32)   
         last_output = last_output.unsqueeze(1)# let (16,32)to(16,1,32)     
+
+        temp01=self.layer_norm(last_output)
+        temp4 = self.linear_32(temp01)
+        temp2=self.relu (temp4)
         # Feed into linear layer and apply activation
         #temp4 = self.linear_32(last_output)#temp4 (16,1,32)
         # #temp4 = temp4.unsqueeze(2)
@@ -93,7 +100,7 @@ class MyRNN(nn.Module):
         #temp4 = temp41.squeeze(2)
  
         # 应用 Dropout 层
-        temp5 = self.dropout(last_output)
+        temp5 = self.dropout(temp2)
         output =self.linear_out(temp5)#output(16,1,7)
         return output,hidden_prev
 #####################training model define#################################
@@ -196,15 +203,15 @@ if __name__ == "__main__":
     print(f"Number of features in one sample: {len(custom_dataset_training[0][1][0])}")# 7 sequence length of features
 
     # para
-    learning_rate = 1e-3
+    learning_rate = 5e-3
     # batch_size = 16
     #epochs = 50
 
     # 设置TensorBoard saving path 
-    log_dir='runs/RNNdemo_experiment_6'
+    log_dir='runs/RNNdemo_experiment_13'
     # using dynamic Para now 1208
-    batch_size_list = [128,256,512]
-    epochs_list=[15000]
+    batch_size_list = [64,128]
+    epochs_list=[200]
     for batch_size in batch_size_list:
         for epochs in epochs_list:
             # path setting
@@ -226,7 +233,7 @@ if __name__ == "__main__":
             loss_fn = nn.MSELoss()  
             # create OP for BQ
             optimizer = optim.Adam(model.parameters(), learning_rate) 
-            scheduler = StepLR(optimizer, step_size=50, gamma=0.9) 
+            scheduler = StepLR(optimizer, step_size=50, gamma=0.8) 
         # 将数据加载器移动到设备
             data_loader_training = DataLoader(custom_dataset_training, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True)
             data_loader_valid = DataLoader(custom_dataset_valid, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True)
